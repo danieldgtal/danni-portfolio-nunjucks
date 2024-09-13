c6a23bfb832c13adabc4169289d967653000dfd6
danieldgtal <danielchiatuiro@gmail.com>
2024-09-13 12:11:17 -0400

    portfolio refactoring in progress

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,13 @@ module.exports = function (eleventyConfig) {
 	// Adding passthrough copy for CSS and img folders within the _includes/head directory
 	eleventyConfig.addPassthroughCopy("css"); // For CSS files
 	eleventyConfig.addPassthroughCopy("img"); // For image files
+	eleventyConfig.addPassthroughCopy("js"); // JavaScript files
+
 	return {
 		dir: {
 			input: ".", // Where your files are stored, adjust accordingly
 			includes: "_includes", // Ensure this matches your folder structure
+			output: "_site", //Output directory
 		},
 		markdownTemplateEngine: "njk",
 		htmlTemplateEngine: "njk",
